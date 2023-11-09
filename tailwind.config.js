d4d08cbe08e8e61a7f4652dc268879925217ebe8
duonglt95 <130416312+duonglt95@users.noreply.github.com>
2023-11-09 16:09:16 +0700

    update o o o

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+     content: ['./layout/*.liquid', './sections/*.liquid'],
+     theme: {
+          extend: {},
+     },
+     plugins: [],
+};
